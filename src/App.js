bc16c319370fa33cb4b9d750949810b12b8c720f
damianaguilarcogan <damian.aguilarcogan@gmail.com>
2020-08-03 20:48:11 -0300

    Proyecto version 3

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Nav from './components/navbar';
 
 function App() {
   return (
     <div className="App">
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -19,6 +23,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Nav />
     </div>
   );
 }
